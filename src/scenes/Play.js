5bb77f52bf808d73489a7698245cc9959cbb9b31
Anna <annaschultz10@gmail.com>
2023-04-18 21:55:00 -0700

    rockets do be patroling

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -49,15 +49,54 @@ class Play extends Phaser.Scene {
             frames: this.anims.generateFrameNumbers("explosion", {start: 0, end: 9, first: 0}),
             frameRate: 30
          });
+
+         //score aspects
+
+         //initalize score
+         this.p1Score = 0;
+         //display score
+         let scoreConfig = {
+            fontFamily : "Courier",
+            fontSize: "28px",
+            backgroundColor: "#F3B141",
+            color: "#843605",
+            align: "right",
+            padding: {
+                top: 5,
+                bottom: 5,
+            },
+            fixedWidth: 100
+         }  
+         this.scoreLeft = this.add.text(borderUISize + borderPadding, borderUISize + borderPadding * 2, this.p1Score, scoreConfig);
+
+         //gameplay aspects
+
+         //GAME OVER flag
+         this.gameOver = false;
+         //60-second play clock
+         scoreConfig.fixedWidth = 0;
+         this.clock = this.time.delayedCall(60000, () => {
+            this.add.text(game.config.width/2, game.config.height/2, "GAME OVER", scoreConfig).setOrigin(0.5);
+            this.add.text(game.config.width/2, game.config.height/2 + 64, "Press (R) to Restart or ← for Menu", scoreConfig).setOrigin(0.5);
+            this.gameOver = true;
+         }, null, this);
+
+
+         
     }
 
     update(){
         //object updates
+        if(this.gameOver && Phaser.Input.Keyboard.JustDown(keyR)){
+            this.scene.restart();
+        }
+        if(!this.gameOver){
         this.starfield.tilePositionX -= 4;
         this.p1Rocket.update();
         this.ship01.update();
         this.ship02.update();
         this.ship03.update();
+        }
         //check collisions
         if(this.checkCollision(this.p1Rocket, this.ship03)) {
             this.p1Rocket.reset();
@@ -70,6 +109,9 @@ class Play extends Phaser.Scene {
         if(this.checkCollision(this.p1Rocket, this.ship01)){
             this.p1Rocket.reset();
             this.shipExplode(this.ship01);
+        }
+        if(this.gameOver && Phaser.Input.Keyboard.JustDown(keyLEFT)){
+            this.scene.start("menuScene");
         }
     }
 
@@ -92,7 +134,13 @@ class Play extends Phaser.Scene {
           ship.reset();                         // reset ship position
           ship.alpha = 1;                       // make ship visible again
           boom.destroy();                       // remove explosion sprite
-        });       
+        });
+        //score add and repaint
+        this.p1Score += ship.points;
+        this.scoreLeft.text = this.p1Score; 
+        //play sound
+        this.sound.play("sfx_explosion");      
       }
+    
 
 }
